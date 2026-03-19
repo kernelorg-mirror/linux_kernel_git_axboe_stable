4bccf34262cebc04066f9070c578284a0528c752
Jens Axboe <axboe@kernel.dk>
2026-03-19 14:29:20 -0600

    io_uring/kbuf: propagate BUF_MORE through early buffer commit path
    commit 418eab7a6f3c002d8e64d6e95ec27118017019af upstream.
    
    When io_should_commit() returns true (eg for non-pollable files), buffer
    commit happens at buffer selection time and sel->buf_list is set to
    NULL. When __io_put_kbufs() generates CQE flags at completion time, it
    calls __io_put_kbuf_ring() which finds a NULL buffer_list and hence
    cannot determine whether the buffer was consumed or not. This means that
    IORING_CQE_F_BUF_MORE is never set for non-pollable input with
    incrementally consumed buffers.
    
    Likewise for io_buffers_select(), which always commits upfront and
    discards the return value of io_kbuf_commit().
    
    Add REQ_F_BUF_MORE to store the result of io_kbuf_commit() during early
    commit. Then __io_put_kbuf_ring() can check this flag and set
    IORING_F_BUF_MORE accordingy.
    
    Reported-by: Martin Michaelis <code@mgjm.de>
    Cc: stable@vger.kernel.org
    Fixes: ae98dbf43d75 ("io_uring/kbuf: add support for incremental buffer consumption")
    Link: https://github.com/axboe/liburing/issues/1553
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/kbuf.c
+++ io_uring/kbuf.c
@@ -205,7 +205,8 @@ static struct io_br_sel io_ring_buffer_select(struct io_kiocb *req, size_t *len,
 		 * the transfer completes (or if we get -EAGAIN and must poll of
 		 * retry).
 		 */
-		io_kbuf_commit(req, sel.buf_list, *len, 1);
+		if (!io_kbuf_commit(req, sel.buf_list, *len, 1))
+			req->flags |= REQ_F_BUF_MORE;
 		sel.buf_list = NULL;
 	}
 	return sel;
@@ -338,7 +339,8 @@ int io_buffers_select(struct io_kiocb *req, struct buf_sel_arg *arg,
 		 */
 		if (ret > 0) {
 			req->flags |= REQ_F_BUFFERS_COMMIT | REQ_F_BL_NO_RECYCLE;
-			io_kbuf_commit(req, sel->buf_list, arg->out_len, ret);
+			if (!io_kbuf_commit(req, sel->buf_list, arg->out_len, ret))
+				req->flags |= REQ_F_BUF_MORE;
 		}
 	} else {
 		ret = io_provided_buffers_select(req, &arg->out_len, sel->buf_list, arg->iovs);
@@ -384,8 +386,10 @@ static inline bool __io_put_kbuf_ring(struct io_kiocb *req,
 
 	if (bl)
 		ret = io_kbuf_commit(req, bl, len, nr);
+	if (ret && (req->flags & REQ_F_BUF_MORE))
+		ret = false;
 
-	req->flags &= ~REQ_F_BUFFER_RING;
+	req->flags &= ~(REQ_F_BUFFER_RING | REQ_F_BUF_MORE);
 	return ret;
 }
 
